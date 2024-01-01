ab9d714bcc013938e47501046857a0c9d22aa39d
dlhorton <147017396+dlhorton@users.noreply.github.com>
2024-01-01 16:47:44 -0600

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "be3e264cb040dab2ab5013d13c51bec8"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "93d75fd6115c4d8c83ab38e4c617863e"
